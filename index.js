9f068a7e9ea1f52a64767c6452583f6cb5e02cdc
Elizabeth T Hallett <ethallett@gmail.com>
2019-12-08 17:01:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,44 @@
-var kittens = [] //define your array here
+var kittens = ['Milo', 'Otis', 'Garfield'] //define your array here
 
 // Add your functions and code here
+function destructivelyAppendKitten(name){
+  kittens.push(name);
+  return(kittens)
+}
+
+function destructivelyPrependKitten(name){
+  kittens.unshift(name);
+  return(kittens)
+
+}
+
+function destructivelyRemoveLastKitten(){
+  kittens.pop();
+  return(kittens)
+
+}
+
+function destructivelyRemoveFirstKitten(){
+  kittens.shift();
+  return(kittens)
+}
+
+function appendKitten(name){
+  kittens = kittens.append(name);
+  return(kittens)
+}
+
+function prependKitten(name){
+ kittens = kittens.prepend(name);
+ return(kittens)
+}
+
+function removeLastKitten(){
+  kittens = kittens.slice(0,2);
+  return(kittens)
+}
+
+function removeFirstKitten(){
+  kittens = kittens.slice(0,0);
+  return(kittens)
+}
